1a7b9390611b09f8ed79fdd1e8238c7e1eb7a4e1
Graciete Gil <graciete.gil@gmail.com>
2024-02-20 21:04:10 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from wsgiref import validate
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint 
+
 from typing import List
 
 SCOPE = [
@@ -35,7 +35,7 @@ def get_sales_data():
             print("Data should be be six numbers, separated by commas.")
             print("Example: 10,20,30,40,50,60\n")
         
-            data_str = input("Enter your data here:")
+            data_str = input("Enter your data here:\n")
             
         
             sales_data = data_str.split(",")
